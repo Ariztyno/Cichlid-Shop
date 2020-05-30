2fa1044e28446f178b9483d33e9aa2bf66b41a09
Tin Nguyen <nguyentrungt98@gmail.com>
2020-05-30 21:14:50 +0700

    Upload file Xuat_lien_he.php

---
+++ ban_hang/chuc_nang/xuat_lien_he.php
@@ -0,0 +1,76 @@
+<!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb2.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Liên hệ với chúng tôi</h2>
+                        <div class="breadcrumb__option">
+                            <a href="index.php">Trang chủ</a>
+                            <span>Liên hệ</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                    <div class="contact__widget">
+                        <span class="icon_phone"></span>
+                        <h4>Điện thoại</h4>
+                        <p>+84 00.10.9999</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                    <div class="contact__widget">
+                        <span class="icon_pin_alt"></span>
+                        <h4>Địa chỉ</h4>
+                        <p>173/5 Điện Biên Phủ, Phường 21, Q.Bình Thạnh, TP.Hồ Chí Minh</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                    <div class="contact__widget">
+                        <span class="icon_clock_alt"></span>
+                        <h4>Giờ mở</h4>
+                        <p>10:00 - 23:00</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                    <div class="contact__widget">
+                        <span class="icon_mail_alt"></span>
+                        <h4>Email</h4>
+                        <p>nguyenminh@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+    <!-- Map Begin -->
+    <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.1445206863777!2d106.71151031428717!3d10.800241261708155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528a56e602479%3A0xc6a10fc9698e831a!2zMTczLzUgxJBp4buHbiBCacOqbiBQaOG7pywgUGjGsOG7nW5nIDIxLCBCw6xuaCBUaOG6oW5oLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1590218476410!5m2!1svi!2s" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <!--<div class="map-inside">
+            <i class="icon_pin"></i>
+            <div class="inside-widget">
+                <h4>New York</h4>
+                <ul>
+                    <li>Phone: +12-345-6789</li>
+                    <li>Add: 16 Creek Ave. Farmingdale, NY</li>
+                </ul>
+            </div>
+        </div>-->
+    </div>
+    <!-- Map End -->
+
+
+<!--<div id="map" style="width:500px;height:500px;">
+	
+
+</div>-->
