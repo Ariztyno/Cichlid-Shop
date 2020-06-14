8bd1706ce97eac23873b6dda498b26319867c38f
Tin Nguyen <nguyentrungt98@gmail.com>
2020-06-14 09:56:14 +0700

    Fix file dangnhap1.php

--- ban_hang/dangnhap1.php
+++ ban_hang/dangnhap1.php
@@ -7,7 +7,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Đăng nhập tài khoản</title>
+		<title>Khách hàng đăng nhập</title>
 		<!-- Tell the browser to be responsive to screen width -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Font Awesome -->
